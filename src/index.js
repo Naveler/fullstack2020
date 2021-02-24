1559331ca4d9c923890362fdc0389c274d1b290e
Naveler <georg.vilja@khk.ee>
2021-02-24 16:22:34 +0200

    exercise 1.6

--- src/index.js
+++ src/index.js
@@ -1,61 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>{props.part.name} {props.part.exercises}</p>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.part1}/>
-      <Part part={props.part2}/>
-      <Part part={props.part3}/>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <p>Number of exercises {props.total}</p>
-  )
-}
-
 const App = () => {
-  // const-definitions
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content part1={course.parts[0]} part2={course.parts[1]} part3={course.parts[2]}/>
-      <Total total={course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises}/>
+      <h1>give feedback</h1>
+      <button onClick={function(){setGood(good+1)}}>good</button>
+      <button onClick={function(){setNeutral(neutral+1)}}>neutral</button>
+      <button onClick={function(){setBad(bad+1)}}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
